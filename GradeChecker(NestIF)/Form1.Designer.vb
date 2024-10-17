6449f6b7cf60c466f21180135061cfd54b9d5c71
Lei <genleiminimrito.o@gmail.com>
2024-10-18 00:19:18 +0800

    Add project files.

---
+++ GradeChecker(NestIF)/Form1.Designer.vb
@@ -0,0 +1,490 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtScore = New TextBox()
+        txtGrade = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        btnCalculate = New Button()
+        GroupBox1 = New GroupBox()
+        Label23 = New Label()
+        Label24 = New Label()
+        Label25 = New Label()
+        Label26 = New Label()
+        Label27 = New Label()
+        Label28 = New Label()
+        Label29 = New Label()
+        Label30 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        Label17 = New Label()
+        Label18 = New Label()
+        Label19 = New Label()
+        Label20 = New Label()
+        Label21 = New Label()
+        Label22 = New Label()
+        Label13 = New Label()
+        Label12 = New Label()
+        Label11 = New Label()
+        Label10 = New Label()
+        Label9 = New Label()
+        Label8 = New Label()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label14 = New Label()
+        Label31 = New Label()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' txtScore
+        ' 
+        txtScore.Font = New Font("Agency FB", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        txtScore.Location = New Point(403, 204)
+        txtScore.Name = "txtScore"
+        txtScore.Size = New Size(100, 23)
+        txtScore.TabIndex = 0
+        txtScore.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' txtGrade
+        ' 
+        txtGrade.Font = New Font("Agency FB", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        txtGrade.Location = New Point(403, 233)
+        txtGrade.Name = "txtGrade"
+        txtGrade.Size = New Size(100, 23)
+        txtGrade.TabIndex = 3
+        txtGrade.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label1
+        ' 
+        Label1.Font = New Font("Agency FB", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.ForeColor = SystemColors.ButtonFace
+        Label1.Location = New Point(295, 205)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(102, 27)
+        Label1.TabIndex = 4
+        Label1.Text = "Enter your  Score:"' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("Agency FB", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.ForeColor = SystemColors.ButtonFace
+        Label2.Location = New Point(355, 234)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(42, 25)
+        Label2.TabIndex = 5
+        Label2.Text = "Grade:"' 
+        ' btnCalculate
+        ' 
+        btnCalculate.BackColor = Color.White
+        btnCalculate.Font = New Font("Agency FB", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        btnCalculate.Location = New Point(403, 262)
+        btnCalculate.Name = "btnCalculate"
+        btnCalculate.Size = New Size(100, 23)
+        btnCalculate.TabIndex = 6
+        btnCalculate.Text = "Calculate Grade"
+        btnCalculate.UseVisualStyleBackColor = False
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = Color.WhiteSmoke
+        GroupBox1.Controls.Add(Label23)
+        GroupBox1.Controls.Add(Label24)
+        GroupBox1.Controls.Add(Label25)
+        GroupBox1.Controls.Add(Label26)
+        GroupBox1.Controls.Add(Label27)
+        GroupBox1.Controls.Add(Label28)
+        GroupBox1.Controls.Add(Label29)
+        GroupBox1.Controls.Add(Label30)
+        GroupBox1.Controls.Add(Label15)
+        GroupBox1.Controls.Add(Label16)
+        GroupBox1.Controls.Add(Label17)
+        GroupBox1.Controls.Add(Label18)
+        GroupBox1.Controls.Add(Label19)
+        GroupBox1.Controls.Add(Label20)
+        GroupBox1.Controls.Add(Label21)
+        GroupBox1.Controls.Add(Label22)
+        GroupBox1.Controls.Add(Label13)
+        GroupBox1.Controls.Add(Label12)
+        GroupBox1.Controls.Add(Label11)
+        GroupBox1.Controls.Add(Label10)
+        GroupBox1.Controls.Add(Label9)
+        GroupBox1.Controls.Add(Label8)
+        GroupBox1.Controls.Add(Label7)
+        GroupBox1.Controls.Add(Label6)
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label14)
+        GroupBox1.ForeColor = Color.Maroon
+        GroupBox1.Location = New Point(12, 72)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(878, 108)
+        GroupBox1.TabIndex = 9
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "RATING GUIDE"' 
+        ' Label23
+        ' 
+        Label23.BackColor = Color.Transparent
+        Label23.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label23.Location = New Point(807, 46)
+        Label23.Name = "Label23"
+        Label23.Size = New Size(57, 24)
+        Label23.TabIndex = 33
+        Label23.Text = "A+"
+        Label23.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label24
+        ' 
+        Label24.BackColor = Color.Transparent
+        Label24.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label24.Location = New Point(749, 46)
+        Label24.Name = "Label24"
+        Label24.Size = New Size(57, 24)
+        Label24.TabIndex = 32
+        Label24.Text = "A"
+        Label24.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label25
+        ' 
+        Label25.BackColor = Color.Transparent
+        Label25.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label25.Location = New Point(686, 46)
+        Label25.Name = "Label25"
+        Label25.Size = New Size(57, 24)
+        Label25.TabIndex = 31
+        Label25.Text = "A-"
+        Label25.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label26
+        ' 
+        Label26.BackColor = Color.Transparent
+        Label26.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label26.Location = New Point(623, 46)
+        Label26.Name = "Label26"
+        Label26.Size = New Size(57, 24)
+        Label26.TabIndex = 30
+        Label26.Text = "B+"
+        Label26.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label27
+        ' 
+        Label27.BackColor = Color.Transparent
+        Label27.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label27.Location = New Point(812, 22)
+        Label27.Name = "Label27"
+        Label27.Size = New Size(57, 24)
+        Label27.TabIndex = 29
+        Label27.Text = "97 to 100"' 
+        ' Label28
+        ' 
+        Label28.BackColor = Color.Transparent
+        Label28.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label28.Location = New Point(749, 22)
+        Label28.Name = "Label28"
+        Label28.Size = New Size(57, 24)
+        Label28.TabIndex = 28
+        Label28.Text = "94 to 96"' 
+        ' Label29
+        ' 
+        Label29.BackColor = Color.Transparent
+        Label29.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label29.Location = New Point(686, 22)
+        Label29.Name = "Label29"
+        Label29.Size = New Size(57, 24)
+        Label29.TabIndex = 27
+        Label29.Text = "90 to 93"' 
+        ' Label30
+        ' 
+        Label30.BackColor = Color.Transparent
+        Label30.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label30.Location = New Point(623, 22)
+        Label30.Name = "Label30"
+        Label30.Size = New Size(57, 24)
+        Label30.TabIndex = 26
+        Label30.Text = "87 to 89"' 
+        ' Label15
+        ' 
+        Label15.BackColor = Color.Transparent
+        Label15.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label15.Location = New Point(555, 46)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(57, 24)
+        Label15.TabIndex = 25
+        Label15.Text = "B"
+        Label15.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label16
+        ' 
+        Label16.BackColor = Color.Transparent
+        Label16.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label16.Location = New Point(497, 46)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(57, 24)
+        Label16.TabIndex = 24
+        Label16.Text = "B-"
+        Label16.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label17
+        ' 
+        Label17.BackColor = Color.Transparent
+        Label17.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label17.Location = New Point(434, 46)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(57, 24)
+        Label17.TabIndex = 23
+        Label17.Text = "D+"
+        Label17.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label18
+        ' 
+        Label18.BackColor = Color.Transparent
+        Label18.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label18.Location = New Point(371, 46)
+        Label18.Name = "Label18"
+        Label18.Size = New Size(57, 24)
+        Label18.TabIndex = 22
+        Label18.Text = "C"
+        Label18.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label19
+        ' 
+        Label19.BackColor = Color.Transparent
+        Label19.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label19.Location = New Point(560, 22)
+        Label19.Name = "Label19"
+        Label19.Size = New Size(57, 24)
+        Label19.TabIndex = 21
+        Label19.Text = "84 to 86"' 
+        ' Label20
+        ' 
+        Label20.BackColor = Color.Transparent
+        Label20.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label20.Location = New Point(497, 22)
+        Label20.Name = "Label20"
+        Label20.Size = New Size(57, 24)
+        Label20.TabIndex = 20
+        Label20.Text = "80 to 83"' 
+        ' Label21
+        ' 
+        Label21.BackColor = Color.Transparent
+        Label21.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label21.Location = New Point(434, 22)
+        Label21.Name = "Label21"
+        Label21.Size = New Size(57, 24)
+        Label21.TabIndex = 19
+        Label21.Text = "77 to 79"' 
+        ' Label22
+        ' 
+        Label22.BackColor = Color.Transparent
+        Label22.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label22.Location = New Point(371, 22)
+        Label22.Name = "Label22"
+        Label22.Size = New Size(57, 24)
+        Label22.TabIndex = 18
+        Label22.Text = "74 to 76"' 
+        ' Label13
+        ' 
+        Label13.BackColor = Color.Transparent
+        Label13.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label13.Location = New Point(308, 46)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(57, 24)
+        Label13.TabIndex = 17
+        Label13.Text = "C-"
+        Label13.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label12
+        ' 
+        Label12.BackColor = Color.Transparent
+        Label12.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label12.Location = New Point(250, 46)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(57, 24)
+        Label12.TabIndex = 16
+        Label12.Text = "D+"
+        Label12.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label11
+        ' 
+        Label11.BackColor = Color.Transparent
+        Label11.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label11.Location = New Point(187, 46)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(57, 24)
+        Label11.TabIndex = 15
+        Label11.Text = "D"
+        Label11.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label10
+        ' 
+        Label10.BackColor = Color.Transparent
+        Label10.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label10.Location = New Point(124, 46)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(57, 24)
+        Label10.TabIndex = 14
+        Label10.Text = "D-"
+        Label10.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label9
+        ' 
+        Label9.BackColor = Color.Transparent
+        Label9.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label9.Location = New Point(61, 46)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(57, 24)
+        Label9.TabIndex = 13
+        Label9.Text = "F"
+        Label9.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label8
+        ' 
+        Label8.BackColor = Color.Transparent
+        Label8.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(313, 22)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(57, 24)
+        Label8.TabIndex = 12
+        Label8.Text = "70 to 73"' 
+        ' Label7
+        ' 
+        Label7.BackColor = Color.Transparent
+        Label7.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(250, 22)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(57, 24)
+        Label7.TabIndex = 11
+        Label7.Text = "67 to 69"' 
+        ' Label6
+        ' 
+        Label6.BackColor = Color.Transparent
+        Label6.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(187, 22)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(57, 24)
+        Label6.TabIndex = 10
+        Label6.Text = "64 to 66"' 
+        ' Label5
+        ' 
+        Label5.BackColor = Color.Transparent
+        Label5.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(124, 22)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(57, 24)
+        Label5.TabIndex = 9
+        Label5.Text = "60 to 63"' 
+        ' Label4
+        ' 
+        Label4.BackColor = Color.Transparent
+        Label4.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(61, 22)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(70, 24)
+        Label4.TabIndex = 8
+        Label4.Text = "60 below"' 
+        ' Label3
+        ' 
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(2, 46)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(53, 24)
+        Label3.TabIndex = 7
+        Label3.Text = "Grade:"' 
+        ' Label14
+        ' 
+        Label14.BackColor = Color.Transparent
+        Label14.Font = New Font("Agency FB", 12F, FontStyle.Bold, GraphicsUnit.Point)
+        Label14.Location = New Point(2, 22)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(53, 24)
+        Label14.TabIndex = 6
+        Label14.Text = "Score:"' 
+        ' Label31
+        ' 
+        Label31.Font = New Font("Agency FB", 36F, FontStyle.Bold, GraphicsUnit.Point)
+        Label31.ForeColor = SystemColors.ButtonFace
+        Label31.Location = New Point(12, 9)
+        Label31.Name = "Label31"
+        Label31.Size = New Size(878, 60)
+        Label31.TabIndex = 10
+        Label31.Text = "GRADE CALCULATOR"' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Maroon
+        ClientSize = New Size(902, 341)
+        Controls.Add(Label31)
+        Controls.Add(GroupBox1)
+        Controls.Add(btnCalculate)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txtGrade)
+        Controls.Add(txtScore)
+        Name = "Form1"
+        Text = "DetailedGradingSystem"
+        GroupBox1.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtScore As TextBox
+    Friend WithEvents txtGrade As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label21 As Label
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label23 As Label
+    Friend WithEvents Label24 As Label
+    Friend WithEvents Label25 As Label
+    Friend WithEvents Label26 As Label
+    Friend WithEvents Label27 As Label
+    Friend WithEvents Label28 As Label
+    Friend WithEvents Label29 As Label
+    Friend WithEvents Label30 As Label
+    Friend WithEvents Label31 As Label
+End Class
---
+++ GradeChecker(NestIF)/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Security.Authentication
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        If Val(txtScore.Text) <= 100 And Val(txtScore.Text) >= 90 Then txtGrade.Text = "A"
+        If txtScore.Text >= 97 Then txtGrade.Text = "A+"
+        If txtScore.Text <= 93 Then
+            txtGrade.Text = "A-"
+        End If
+        If Val(txtScore.Text) <= 89 And Val(txtScore.Text) >= 80 Then
+            txtGrade.Text = "B"
+
+            If txtScore.Text >= 87 Then txtGrade.Text = "B+"
+            If txtScore.Text <= 83 Then txtGrade.Text = "B-"
+        End If
+        If Val(txtScore.Text) <= 79 And Val(txtScore.Text) >= 70 Then
+            txtGrade.Text = "C"
+
+            If txtScore.Text >= 77 Then txtGrade.Text = "C+"
+            If txtScore.Text <= 73 Then txtGrade.Text = "C-"
+        End If
+        If Val(txtScore.Text) <= 69 And Val(txtScore.Text) >= 60 Then
+            txtGrade.Text = "D"
+
+            If txtScore.Text >= 67 Then txtGrade.Text = "D+"
+            If txtScore.Text <= 63 Then txtGrade.Text = "D-"
+        End If
+        If Val(txtScore.Text) < 60 Then
+            txtGrade.Text = "F"
+        End If
+
+
+
+
+
+
+
+
+
+    End Sub
+
+
+End Class
